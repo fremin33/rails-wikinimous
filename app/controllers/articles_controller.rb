68f8dc1ed77242b646406f5a20252238258f1923
florian fremin <florian.fremin33@gmail.com>
2017-02-14 17:47:13 +0100

    add databse with faker

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,35 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+  end
+
+  def destroy
+    @article.destroy
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
